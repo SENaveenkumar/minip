c93be54829cc159b2f3aeca36bd5f8d800e6fe63
naveenkumarse <naveenelango.se@gmail.com>
2022-05-30 10:59:56 +0530

    commit changes

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.post("/api/student", (req, res) => {
   console.log(req.body);
   con.connect(function (err) {
     if (err) throw err;
-    var sql = `INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
-      VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway')`;
+    var sql = `INSERT INTO Student (NAME, FATHER_NAME, SSLC_MARK, HSC_MARK,ACCOMODATION,COURSE,PRESENT_COUNT)
+      VALUES ('${req.body.name}', '${req.body.fname}', '${req.body.sslc}', '${req.body.hsc}', '${req.body.am}','${req.body.course}','${req.body.pre_count}')`;
 
     con.query(sql, function (err, result) {
       if (err) throw err;
@@ -33,6 +33,35 @@ app.post("/api/student", (req, res) => {
   });
 });
 
+app.post("/api/signup/", (req, res) => {
+  console.log(req.body);
+
+  var sql = `INSERT INTO USERS (NAME, MAIL, USERNAME, DESIGNATION,PWD)
+      VALUES ('${req.body.name}', '${req.body.mail}', '${req.body.username}', '${req.body.designation}', '${req.body.pwd}')`;
+
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    const hasRows = result.affectedRows === 1;
+    res.json({ success: hasRows  });
+  });
+
+});
+
+app.post("/api/login", (req, res) => {
+  console.log(req.body);
+
+  var sql = 'SELECT * FROM USERS WHERE USERNAME="' + req.body.username + '" AND PWD="' + req.body.pwd + '"';
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    const hasRows = result.length === 1;
+    res.json({ success: hasRows  });
+  });
+
+});
+
 app.listen(9000, () => {
-  console.log("Server Started Successfully");
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Server Started Successfully");
+  });
 });
